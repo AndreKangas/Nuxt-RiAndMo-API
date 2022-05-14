349a41cc55987d15cbc8114ca59afc36ebbd30b2
Andre Kangas <andre.kangas@tptlive.ee>
2022-05-14 22:30:13 +0300

    created nuxt API read from class, a bit broken

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,84 @@
 <template>
-  <h1>Hello Nuxt</h1>
+  <div>
+    <div class="text-center">
+      <v-pagination
+        v-model="page"
+        :length="$store.state.pagination.pages"
+        :total-visible="7"
+        @previous="prev"
+        @next="next"
+        @input="getPage"
+      >
+      </v-pagination>
+    </div>
+
+    <v-row>
+      <v-col
+        :cols="3"
+        v-for="result in $store.state.results"
+        :key="'result-' + result.id"
+      >
+        <v-card elevation="2">
+          <v-img :src="result.image"></v-img>
+          <v-card-title
+            ><router-link :to="'/character/' + result.id">{{
+              result.name
+            }}</router-link></v-card-title
+          >
+          <v-card-text>
+            <ul>
+              <li><b>Status:</b>{{ result.name }}</li>
+              <li><b>Gender:</b>{{ result.gender }}</li>
+              <li><b>Species:</b>{{ result.species }}</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
+  created() {
+    if (this.$route.query.page) {
+      this.$store.commit("SET_PAGE", parseInt(this.$route.query.page));
+    }
+    this.$store.dispatch("getPage", this.$store.state.page);
+  },
+  beforeRouteUpdate(to, from, next) {
+    let page = 1;
+    if (to.query.page) {
+      this.page = parseInt(to.query.page);
+    }
+    $store.commit("SET_PAGE", page);
+    next();
+  },
 
-}
+  methods: {
+    next() {
+      this.getPage(this.page + 1);
+    },
+    prev() {
+      this.getPage(this.page - 1);
+    },
+    getPage(page) {
+      this.$router.push("/?page=" + page);
+      this.$store.dispatch("getPage", page);
+    }
+  },
+
+  computed: {
+    page: {
+      set(value) {
+        this.$store.commit("SET_PAGE", value);
+      },
+      get() {
+        return this.$store.state.page;
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
